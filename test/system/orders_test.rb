69ad51b43f63abff5b80c73cf1d843e3962046ed
sarah wang <sarahwang47@gmail.com>
2020-09-22 10:15:21 +0800

    add action mailer

--- test/system/orders_test.rb
+++ test/system/orders_test.rb
@@ -19,16 +19,21 @@ class OrdersTest < ApplicationSystemTestCase
   end
 
   test "check routing number" do
-    click_on "New Order"
+    visit store_index_url
 
-    fill_in " pay type", with: @order. pay_type
-    fill_in "Address", with: @order.address
-    fill_in "Email", with: @order.email
-    fill_in "Name", with: @order.name
-    click_on "Create Order"
+    click on 'Add to Cart' , match: :first
 
-    assert_text "Order was successfully created"
-    click_on "Back"
+    click on 'Check out'
+
+    fill_in "order_name", with: 'Dave Thomas'
+    fill_in "order_address", with: '123 Main Street'
+    fill_in "order_email", with: 'dave@exemple.com'
+
+    assert_no_selector "#order_routing_number"
+
+    select 'Check' , from: 'Pay type'
+    assert_selector "#order_routing_number"
+
   end
 
   test "updating a Order" do
