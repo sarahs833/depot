d74a575d7737b237c16cb17b3bf61c41ef608280
sarah wang <sarahwang47@gmail.com>
2020-10-02 20:13:32 +0800

    added test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: params[:name])
-    if user.authenticate(params[:password])
+    if user&.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to admin_url
     else
